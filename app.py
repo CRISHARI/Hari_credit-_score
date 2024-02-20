b3631e0185034d1062c8d37854b35a44b35a97aa
CRISHARI <141857936+CRISHARI@users.noreply.github.com>
2024-02-20 08:35:42 +0000

    one

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import pickle
 import numpy as np
 from flask import Flask, request, render_template
+import gzip
+
 #import pandas as pd
 
+
+
+
+
 app = Flask(__name__)
-model = pickle.load(open('credit.pkl','rb'))
+with gzip.open("model.pickle","rb") as file:
+    load_model = pickle.load(file)
 @app.route('/')
 def index():
 
@@ -14,40 +21,43 @@ def index():
 def predict():
  
         if request.method == 'POST':
+            
            
-           Age =int(request.form["Age"])
+        
            Annual_Income =float(request.form["Annual_Income"])
-           Num_Bank_Accounts =int(request.form["Num_Bank_Accounts"])
-           Num_Credit_Card = int(request.form["Num_Credit_Card"])
+           Age = float(request.form["Age"])
+           Num_Bank_Accounts =float(request.form["Num_Bank_Accounts"])
+           Num_Credit_Card = float(request.form["Num_Credit_Card"])
+           Num_of_Loan = float(request.form["Num_of_Loan"])
            Interest_Rate = float(request.form["Interest_Rate"])
-           Num_of_Loan = int(request.form["Num_of_Loan"])
+           Monthly_Balance = float(request.form["Monthly_Balance"])
+           Outstanding_Debt =float(request.form["Outstanding_Debt"])
            Delay_from_due_date = float(request.form["Delay_from_due_date"])
            Num_of_Delayed_Payment = float(request.form["Num_of_Delayed_Payment"])
-           Credit_Mix = request.form["Credit_Mix"]
-           Outstanding_Debt =float(request.form["Outstanding_Debt"])
-           Credit_History_Age = int(request.form["Credit_History_Age"])
+           Changed_Credit_Limit =float(request.form["Changed_Credit_Limit"])
+           Num_Credit_Inquiries = float(request.form["Num_Credit_Inquiries"])
+           Credit_History_Age = float(request.form["Credit_History_Age"])
+           Payment_of_Min_Amount = request.form["Payment_of_Min_Amount"]
            Payment_Behaviour =request.form["Payment_Behaviour"]
-           Monthly_Balance = float(request.form["Monthly_Balance"])
-           Credit_History_Age =int(request.form["Credit_History_Age"])
-           Num_Credit_Inquiries =int(request.form["Num_Credit_Inquiries"])
-           Payment_of_Min_Amount = float(request.form["Payment_of_Min_Amount"])
-           Changed_Credit_Limit = float(request.form["Changed_Credit_Limit"])
-
-
-           prediction = model.predict(np.array([[Age,Annual_Income,Num_Bank_Accounts,Num_Credit_Card,
-                                              Interest_Rate,Num_of_Loan,Delay_from_due_date,Num_of_Delayed_Payment,
-                                              Credit_Mix,Outstanding_Debt,Credit_History_Age,Payment_of_Min_Amount,Num_Credit_Inquiries,
-                                              Changed_Credit_Limit,Payment_Behaviour,Monthly_Balance]]))[0]
+           Credit_Mix = request.form["Credit_Mix"]
+           
+           
+           prediction = load_model.predict(np.array([[Annual_Income,Age,Num_Bank_Accounts,Num_Credit_Card,
+                                              Num_of_Loan,Interest_Rate,Monthly_Balance,Outstanding_Debt,
+                                              Delay_from_due_date,Num_of_Delayed_Payment,Credit_Mix,
+                                              Changed_Credit_Limit,Num_Credit_Inquiries,Credit_History_Age,
+                                              Payment_Behaviour,Payment_of_Min_Amount]]))[0]
+                                             
            output = int(prediction)
            if (output == 0):
          
-               return render_template("result.html",prediction_text=" Credit Score is Good, predicted value is: ""{}".format(output))
+               return render_template("result.html",prediction_text=" Credit Score is classified as Good")
            
            elif (output==1):
-                return render_template("result.html",prediction_text="Credit Score is Poor, predicted value is: ""{}".format(output))
+                return render_template("result.html",prediction_text="Credit Score is classified as Poor")
         
            else :
-                return render_template("result.html",prediction_text= "Credit Score is Standard, predicted value is :""{}".format(output))
+                return render_template("result.html",prediction_text= "Credit Score is classified as Standard")
                                         
                                  
           
--- model.py
+++ model.py
@@ -1,74 +1,67 @@
 import numpy as np
 import pandas as pd
-import gzip
-import pickle
-from sklearn.preprocessing import LabelEncoder
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+import pickle
+import gzip
 
+data= pd.read_csv("credit.csv")
 
-
-data = pd.read_csv("credit.csv")
-
-
-#converting float to int
 data["Age"] = data["Age"].astype(int)
 data["Num_of_Loan"] = data["Num_of_Loan"].astype(int)
 data["Num_Bank_Accounts"] =data["Num_Bank_Accounts"].astype(int)
 data["Credit_History_Age"] = data["Credit_History_Age"].astype(int)
 data["Num_Credit_Inquiries"] = data["Num_Credit_Inquiries"].astype(int)
 data["Num_Credit_Card"] = data["Num_Credit_Card"].astype(int)
-data["Interest_Rate"] = data["Interest_Rate"].astype(int)
 data["Delay_from_due_date"] = data["Delay_from_due_date"].astype(int)
+data["Num_of_Delayed_Payment"] = data["Num_of_Delayed_Payment"].astype(int)
+data["Changed_Credit_Limit"] = data["Changed_Credit_Limit"].astype(int)
+
+train=data.drop(["ID","Customer_ID","SSN","Month","Name","Type_of_Loan","Occupation",
+                 "Amount_invested_monthly","Credit_Utilization_Ratio","Total_EMI_per_month","Monthly_Inhand_Salary"],axis=1)
 
 
-#drop irrelevent features
-data1 = data.drop(columns=['ID','Customer_ID','Name','Month','Type_of_Loan','SSN','Amount_invested_monthly','Occupation','Credit_Utilization_Ratio','Total_EMI_per_month','Monthly_Inhand_Salary'])
+CreditScore ={"Good" :0,"Poor":1,"Standard":2}
+train["Credit_Score"] = train["Credit_Score"].map(CreditScore)
+PaymentBehaviour={"Low_spent_Small_value_payments":5,"High_spent_Medium_value_payments":1,
+                   "High_spent_Large_value_payments":0,"Low_spent_Medium_value_payments":4,
+                   "High_spent_Small_value_payments":2,"Low_spent_Large_value_payments":3}
+train["Payment_Behaviour"] =train["Payment_Behaviour"].map(PaymentBehaviour)
+PaymentofMinAmount ={"Yes":2,"No":1,"NM":0}
+train["Payment_of_Min_Amount"]=train["Payment_of_Min_Amount"].map(PaymentofMinAmount)
+CreditMix = {"Standard":2,"Good":1,"Bad":0}
+train["Credit_Mix"]=train["Credit_Mix"].map(CreditMix)
 
 
-#label encoding
-from sklearn.preprocessing import LabelEncoder
-le= LabelEncoder()
-data1['Credit_Score'] = le.fit_transform(data1['Credit_Score'])
-data1['Payment_of_Min_Amount'] = le.fit_transform(data1['Payment_of_Min_Amount'])
-data1['Credit_Mix'] = le.fit_transform(data1['Credit_Mix'])
-data1['Payment_Behaviour'] = le.fit_transform(data1['Payment_Behaviour'])
+X = train.drop(["Credit_Score"],axis=1)
+Y = pd.DataFrame(train["Credit_Score"])
+
+smote = SMOTE()
+X,Y = smote.fit_resample(X,Y)
 
 
 
+scaler_x = MinMaxScaler()
+X_scale= scaler_x.fit_transform(X)
 
-# Split the data into features (X) and target variable (y)
+Y= np.squeeze(Y)
 
-X = data1.drop("Credit_Score",axis=1)
-Y = pd.DataFrame(data1["Credit_Score"])
+x_train,x_test,y_train,y_test = train_test_split(X_scale,Y, test_size = 0.3, random_state=42)
 
-from imblearn.over_sampling import SMOTE
-sm = SMOTE()
-X,Y = sm.fit_resample(X, Y)#over-sampling the data
+rf_cls =RandomForestClassifier()
 
-from sklearn.preprocessing import MinMaxScaler#Perfrom MinMax Preprocessing
-scaler = MinMaxScaler()
+model_rf = rf_cls.fit(x_train,y_train)
 
-# Fit the scaler on the data and transform it
-scaled_data = scaler.fit_transform(X)
+y_pred_rf = model_rf.predict(x_test)
 
+pipeline = make_pipeline(MinMaxScaler(),RandomForestClassifier())
 
-from sklearn.model_selection import train_test_split
-#split the data into training, validation, and testing sets for model development.
+model = pipeline.fit(x_train, y_train)
+y_pred = model.predict(x_test)
 
-Y= np.squeeze(Y)#squeeze() function is used to remove single-dimensional entries from the shape of an array.
-
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size = 0.20, random_state=42,)
-
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-rf_model = RandomForestClassifier(n_estimators=1000,random_state=42)
-model=rf_model.fit(X_train, Y_train)
-rf_predictions= model.predict(X_test)
-rf_Acc = accuracy_score(Y_test, rf_predictions)
-
-pickle.dump(model,open('credit.pkl','wb'))
-
- 
+filename="model.pickle"
+with gzip.open(filename,"wb") as file:
+   pickle.dump(model,file)
